53e1e527e46fca6da590897e5d05651bc103413f
gbmyt <gbmytabc1@gmail.com>
2022-10-18 19:51:07 -0400

    First pass attempt at passing props from parent to children components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+// import VideoList from './components/VideoList.js';
+// import exampleVideoData from './data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,12 +13,12 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div>{props.videoList}</div>
       </div>
     </div>
   </div>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
+// `var` declarations will only exist globally where explicitly define
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+// import VideoList from './components/VideoList.js';
+//import exampleVideoData from './data/exampleVideoData.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
